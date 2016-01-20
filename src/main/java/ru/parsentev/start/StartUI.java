b990e7fb52b996eb9672275d8c6dd3e4446a4f33
BogdanAndrusyak <bogdan.andrusyak@yahoo.com>
2016-01-20 15:58:30 +0200

    fix

--- src/main/java/ru/parsentev/start/StartUI.java
+++ src/main/java/ru/parsentev/start/StartUI.java
@@ -21,92 +21,10 @@ public class StartUI {
 			int key = Integer.valueOf(input.ask("Select: "));
 			menu.select(key);
 		} while(!"y".equals(this.input.ask("Exit?(y): ")));
-		
-		// Old menu...
-		/*Menu menu = new Menu(input);
-
-		menu.addEntry(new MenuEntry("Add Task") {
-			public void run() {
-				tracker.add(add());
-			}
-		});
-
-		menu.addEntry(new MenuEntry("Find by name Task") {
-			public void run() {
-				Item item = findByName();
-			}
-		});
-
-		menu.addEntry(new MenuEntry("Add comment to Task") {
-			public void run() {
-				addComment();
-			}
-		});
-
-		menu.addEntry(new MenuEntry("Edit Task") {
-			public void run() {
-				edit();
-			}
-		});
-
-		menu.addEntry(new MenuEntry("Show all tasks") {
-			public void run() {
-				showAllItems();
-			}
-		});
-
-		menu.addEntry(new MenuEntry("Delete Task") {
-			public void run() {
-				delete();
-			}
-		});
-		
-		menu.run();*/
 	}
 
 	public static void main(String[] args) {
 		Input input = new ConsoleInput();
 		new StartUI(input).init();
 	}
-	// Old methods...
-	/*public Item add() {
-		String name = input.ask("Please enter the task's name: ");
-		String desc = input.ask("Please enter the task's description: ");
-
-		return new Task(name, desc);
-	}
-
-	public Item findByName() {
-		String name = input.ask("Please enter the task's name: ");
-		return tracker.findByName(name);
-	}
-
-	public void edit() {
-		String oldName = input.ask("Please enter the old task's name: ");
-		String newName = input.ask("Please enter the new task's name: ");
-		String newDesc = input.ask("Please enter the new task's description: ");
-
-		tracker.edit();
-	}
-
-	public void delete() {
-		String name = input.ask("Please enter the task's name to delete: ");
-		tracker.delete(tracker.findByName(name).getId());
-	}
-
-	public void addComment() {
-		String name = input.ask("Please enter the task's name to add comment: ");
-		String commentDesc = input.ask("Please enter the comment: ");
-
-		tracker.addComment(tracker.findByName(name), new Comment(commentDesc, new Date().getTime()));
-	}
-
-	public void showAllItems() {
-		String answer = input.ask("Show all tasks yes/no: ");
-		if (answer.equals("yes")) {
-			for (Item item : tracker.getAll()) {
-				System.out.println(item.getName());
-			}
-		}
-	}*/
 }
