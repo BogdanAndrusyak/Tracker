035eca0caf947469a36d200513c2afd8608d3074
BogdanAndrusyak <bogdan.andrusyakt@gmail.com>
2016-04-11 22:32:45 +0300

    #7.1 Sql

---
+++ src/main/java/ru/parsentev/SQL Scripts/createTables.sql
@@ -0,0 +1,75 @@
+﻿create table roles (
+	id serial primary key,
+	name text
+);
+
+create table permissions (
+	id serial primary key,
+	name text,
+	description text
+);
+
+create table role_permission (
+	id serial primary key,
+	role_id serial,
+	foreign key (role_id) references roles(id),
+	permission_id serial,
+	foreign key (permission_id) references permissions(id)
+);
+
+create table users (
+	id serial primary key,
+	name text,
+	role_id serial,
+	foreign key (role_id) references roles(id)
+);
+
+create table items (
+	id serial primary key,
+	name text,
+	description text,
+	create_date timestamp not null default now(),
+	user_id serial,
+	foreign key (user_id) references users(id)
+); 
+
+create table comments (
+	id serial primary key,
+	description text,
+	create_date timestamp not null default now(),
+	item_id serial,
+	foreign key (item_id) references items(id)
+);
+
+create table files (
+	id serial primary key,
+	file bytea,
+	item_id serial,
+	foreign key (item_id) references items(id)
+);
+
+create table statuses (
+	id serial primary key,
+	name text
+);
+
+create table Status_Item (
+	id serial primary key,
+	status_id serial,
+	foreign key (status_id) references statuses(id),
+	item_id serial,
+	foreign key (item_id) references items(id)
+);
+
+create table categories (
+	id serial primary key,
+	name text
+);
+
+create table Category_Item (
+	id serial primary key,
+	category_id serial,
+	foreign key (category_id) references categories(id),
+	item_id serial,
+	foreign key (item_id) references items(id)
+);
---
+++ src/main/java/ru/parsentev/SQL Scripts/insertData.sql
@@ -0,0 +1,30 @@
+﻿insert into roles(name) values ('admin');
+insert into roles(name) values ('guest');
+
+insert into permissions(name, description) values ('add_item', 'ability to add items'); 
+insert into permissions(name, description) values ('edit_item', 'ability to change items');
+insert into permissions(name, description) values ('view_item', 'ability to view items');
+
+insert into role_permission(role_id, permission_id) values (1, 1);
+insert into role_permission(role_id, permission_id) values (1, 2);
+insert into role_permission(role_id, permission_id) values (1, 3);
+insert into role_permission(role_id, permission_id) values (2, 1);
+insert into role_permission(role_id, permission_id) values (2, 3);
+
+insert into users(name, role_id) values ('Petr Arsentev', 1);
+insert into users(name, role_id) values ('Bogdan Andrusyak', 2);
+
+insert into items(name, description, user_id) values ('Item1', 'write database creation script', 1);
+
+insert into comments(description, item_id) values ('easy', 1);
+
+--todo insert files to item
+
+insert into statuses(name) values('active');
+insert into statuses(name) values('inactive');
+
+insert into status_item(status_id, item_id) values(1, 1);
+
+insert into categories(name) values ('must_do');
+
+insert into category_item(category_id, item_id) values (1, 1);
