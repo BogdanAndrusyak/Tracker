c492b95d47daa98d0b55189f6605c0b964f5c1d4
Bogdan <bogdan.andrusyakt@gmail.com>
2016-04-20 15:12:07 +0300

    #7.3

---
+++ src/main/java/ru/parsentev/SQL Scripts/queryFilters.sql
@@ -0,0 +1,6 @@
+﻿--select * from items as i where i.name = 'Item1';
+--select * from items as i where i.id > 0;
+--select * from items as i where i.id between 1 and 3;
+--select * from items as i where i.id in(1, 3); 
+--select * from items as i where i.name like '%Item%'; 
+select i.name, i.description, c.description as comment from items as i inner join comments as c on i.id = c.item_id;
